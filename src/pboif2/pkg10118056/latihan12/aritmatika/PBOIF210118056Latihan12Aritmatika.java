342a5b85ea1b7633781a360f4bf16eaaae488a2c
Mirrazibrahim <55575435+Mirrazibrahim@users.noreply.github.com>
2019-09-26 13:09:17 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118056/latihan12/aritmatika/PBOIF210118056Latihan12Aritmatika.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118056.latihan12.aritmatika;
+
+import javax.xml.transform.Source;
+
+/**
+ *
+ * @author 
+ * NAMA     : Mirrazibrahim
+ * KELAS    : IF-2
+ * NIM      : 10118056
+ * Deskripsi Program : Program ini berisi program untuk menampilkan Aritmatika
+ */
+public class PBOIF210118056Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    int a = 10;
+    int b = 20;
+    int c = 25;
+    int d = 25;
+    System.out.println("a + b =" + (a + b));
+    System.out.println("a - b = " + (a - b));
+    System.out.println("a * b = " + (a * b));
+    System.out.println("a / b = " + (a / b));
+    System.out.println("b % a = " + (b % a));
+    System.out.println("c % a = " + (c % a));
+    System.out.println("a++ = " + (a++));
+    System.out.println("b-- = " + (b--));
+    System.out.println("d++ = " + (d++));
+    System.out.println("++d = " + (++d));
+    
+    
+    
+    
+    }
+}
